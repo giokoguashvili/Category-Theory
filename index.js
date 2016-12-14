c4e87d7428b4d33a04c0b0ec56de5082e3325d36
kogoia <kogo27@gmail.com>
2016-12-14 15:39:13 +0400

    overwrite Array prototype reduce function

---
+++ index.js
@@ -0,0 +1,15 @@
+Array.prototype.reduce = function(reducer, initialValue) {
+    var acc = initialValue;
+    for (let i = 0; i < this.length; i ++) {
+       acc = reducer(acc, this[i]);
+    }
+    return acc || initialValue;
+};
+
+var data = [1, 2, 3].reduce((acc, val) => { 
+    acc.push(val * 2);
+    return acc;
+}, []);
+
+
+console.log(data);
