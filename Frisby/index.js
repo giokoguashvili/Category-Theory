b085c5d481fc4f0ed2821722a847907ddb3d3acd
kogoia <kogo27@gmail.com>
2017-01-13 18:56:01 +0400

    add nextCharForNumberString method

---
+++ Frisby/index.js
@@ -0,0 +1,10 @@
+const nextCharForNumberString = str => {
+    const trimmed = str.trim();
+    const number = parseInt(trimmed);
+    const nextNumber = number + 1;
+    return String.fromCharCode(nextNumber);
+}
+
+
+
+console.log(nextCharForNumberString('  64 '));
