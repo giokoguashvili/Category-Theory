f0d046e314a46f01a4b0d008c8046f9b558b85c2
kogoia <kogo27@gmail.com>
2017-01-13 18:59:18 +0400

    change method with chaining

--- Frisby/index.js
+++ Frisby/index.js
@@ -5,6 +5,13 @@ const nextCharForNumberString = str => {
     return String.fromCharCode(nextNumber);
 }
 
-
+const nextCharForNumberString_2 = str => {
+    return [str]
+        .map(s => s.trim())
+        .map(ts => parseInt(ts))
+        .map(n => n + 1)
+        .map(nn => String.fromCharCode(nn))
+}
 
 console.log(nextCharForNumberString('  64 '));
+console.log(nextCharForNumberString_2('  66 '));
