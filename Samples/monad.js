efe3e872b53afa7cde37c66f35aa8f598e2e69cd
kogoia <kogo27@gmail.com>
2017-01-21 16:41:51 +0400

    added monad primitive realization

---
+++ Samples/monad.js
@@ -0,0 +1,43 @@
+Array.prototype.concatAll = function() {
+    var result = [];
+    this.forEach((item) => {
+        result = result.concat(item);
+    });
+    return result;
+};
+
+function output() { return Array.from(arguments).map(i => console.log(i)); }
+
+const fnA = (number) =>
+    number.toString().split('');
+
+const fnB = (numbers) =>
+    numbers.map(n => fnA(n)).concatAll();
+
+
+
+output(
+    // fnA :: a -> [b]
+    fnA(10), // ['1', '0']
+
+    // funB :: [a] -> [b]
+    fnB([10,11,12]) // ['1','0','1','1','1','2']
+); 
+
+//monad :: (a -> [b]) -> ([a] -> [b])
+function monad(fnA) {
+    return function(numbers) {
+        return numbers
+            .map(n => fnA(n))
+            .concatAll();
+    }
+}
+
+Array.prototype.bind = function(fnA) {
+    return monad(fnA)(this);
+}
+
+output(
+    [10,11,12,13].bind(fnA)
+);
+
